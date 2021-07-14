1e3518c2b64731ad62ef633a5e3e5f02c3b8c87e
Kevin Cai <75402062+Kev-in123@users.noreply.github.com>
2021-07-13 21:39:04 -0400

    Add files via upload

---
+++ kahoot.py
@@ -0,0 +1,55 @@
+import time
+import string
+import random
+import selenium
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+web = webdriver.Firefox(executable_path="C:\\geckodriver.exe")
+web.get("https://kahoot.it")
+
+buttons=['//*[@id="root"]/div/main/div[2]/div/div/button[1]',
+         '//*[@id="root"]/div/main/div[2]/div/div/button[2]',
+         '//*[@id="root"]/div/main/div[2]/div/div/button[3]',
+         '//*[@id="root"]/div/main/div[2]/div/div/button[4]']
+
+class KahootBot():
+    def __init__(self, game_pin, name = None):
+        self.game_pin = game_pin
+        self.name = name or "".join([random.choice(f"{string.ascii_uppercase}{string.digits}") for n in range(5)])
+
+    def start(self):
+    
+        
+
+        while True:
+            try:
+                id_textbox = web.find_element_by_xpath('//*[@id="game-input"]') 
+                break
+            except selenium.common.exceptions.NoSuchElementException:
+                time.sleep(0.1)
+
+        id_textbox.send_keys(self.game_pin)
+        id_textbox.send_keys(Keys.ENTER)
+
+
+        while True:
+            try:
+                name_textbox = web.find_element_by_xpath('//*[@id="nickname"]') 
+                break
+            except selenium.common.exceptions.NoSuchElementException:
+                time.sleep(0.1)
+
+        name_textbox.send_keys(self.name)
+        name_textbox.send_keys(Keys.ENTER)
+
+        while True:
+            try:
+                to_click = random.choice(buttons)
+                button_box = web.find_element_by_xpath(to_click)
+                button_box.click()
+            except selenium.common.exceptions.NoSuchElementException:
+                time.sleep(0.1)
+
+    
+
