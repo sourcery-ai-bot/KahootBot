3c8a5bac57f8329763463bda9091d9e425fdc3fa
Kevin Cai <75402062+Kev-in123@users.noreply.github.com>
2021-07-21 10:38:10 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,4 +1,4 @@
 from KahootBot import KahootBot
 
-kahoot_bot = KahootBot(game_pin="", name="") # don't NEED to specify name because a random one will be gererated if you dont. Just change game pin
+kahoot_bot = KahootBot(game_pin="", name="") # don't NEED to specify name because a random one will be gererated if you don't, just change the game pin
 kahoot_bot.start()
